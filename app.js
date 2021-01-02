6caa8c4c3c486cece299607e480840a095d75608
zhen <zhen.yang@hotmail.fr>
2021-01-02 17:17:08 +0100

    maj

--- app.js
+++ app.js
@@ -2,24 +2,31 @@ const express = require("express");
 const app = new express();
 
 const routes = require("./routes/routes.js");
-const db = require("./model/db.js");
+//const db = require("./model/db.js");
 //db.getConnection();
 
-const fs = require(‘fs’);
+const fs = require("fs");
 const certFileBuf = fs.readFileSync("./docs/ec2-mongodb.pem");
 
 // Set up mongoose connection
-var mongoose = require(‘mongoose’);
-var dev_db_url = "mongodb://userdemo:userdemo@docdb-2021-01-01-16-23-03.cluster-cfo18mts2t3o.eu-west-3.docdb.amazonaws.com:27017/dadi?ssl=true&ssl_ca_certs=rds-combined-ca-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false";
+var mongoose = require("mongoose");
+var dev_db_url = "mongodb://userdemo:userdemo@docdb-2021-01-01-16-23-03.cluster-cfo18mts2t3o.eu-west-3.docdb.amazonaws.com:27017/?ssl=true&ssl_ca_certs=rds-combined-ca-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false";
 var mongoDB = process.env.MONGODB_URI || dev_db_url;
 var options = {
-	sslCA: certFileBuf
+	sslCA: certFileBuf,
+	useNewUrlParser: true,
+	useUnifiedTopology:true
 };
 
-mongoose.connect(mongoDB,options);
-mongoose.Promise = global.Promise;
-var db = mongoose.connection;
-db.on("error", console.error.bind(console, "MongoDB connection error:"));
+mongoose.connect(mongoDB,options,function(err,client){
+	if(err)console.log(err);
+	else{
+		var db = mongoose.connection;
+		db.on("error", console.error.bind(console, "MongoDB connection error:"));
+	}
+});
+
+
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -27,5 +34,5 @@ app.use(bodyParser.json())
 
 app.use('/api',routes);
 
-const port = process.env.PORT || '0.0.0.0';
+const port = process.env.PORT || 8000;
 app.listen(port); 
