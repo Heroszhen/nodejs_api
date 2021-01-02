d97eb3d772b291a918250b9bf9c7d10e61930940
zhen <zhen.yang@hotmail.fr>
2021-01-02 16:27:44 +0100

    maj

--- model/db.js
+++ model/db.js
@@ -3,12 +3,15 @@ const mongoose = require("mongoose");
 var url = "mongodb://userdemo:userdemo@docdb-2021-01-01-16-23-03.cluster-cfo18mts2t3o.eu-west-3.docdb.amazonaws.com:27017/?ssl=true&ssl_ca_certs=rds-combined-ca-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false";
 
 exports.getConnection = function(){
-	mongoose.connect(url,{useNewUrlParser: true,useUnifiedTopology:true});
-	var db = mongoose.connection;
-	db.on('error', console.error.bind(console, 'connection error:'));
-	db.once('open', function() {
-	  console.log("Connected");
-	});
+	mongoose.connect(url,{useNewUrlParser: true,useUnifiedTopology:true},()=>{
+		var db = mongoose.connection;
+		db.on('error', console.error.bind(console, 'connection error:'));
+		db.once('open', function() {
+		  console.log("Connected");
+		});
+	})
+	.catch(err=>console.log(err));
+	
 };
 
 
