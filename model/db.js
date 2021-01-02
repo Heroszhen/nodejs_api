ce927df89c5e3475374c0699fd4a13549bce7e85
zhen <zhen.yang@hotmail.fr>
2021-01-02 15:59:46 +0100

    maj

--- model/db.js
+++ model/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
-var url = 'mongodb://localhost:27017/dadi';
-//var url = "mongodb://testdocumentdb:testdocumentdb@docdb-2020-12-27-16-02-20.cluster-cfo18mts2t3o.eu-west-3.docdb.amazonaws.com:27017/?ssl=true&ssl_ca_certs=rds-combined-ca-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false";
+//var url = 'mongodb://localhost:27017/dadi';
+var url = "mongodb://userdemo:userdemo@docdb-2021-01-01-16-23-03.cluster-cfo18mts2t3o.eu-west-3.docdb.amazonaws.com:27017/?ssl=true&ssl_ca_certs=rds-combined-ca-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false";
 
 exports.getConnection = function(){
 	mongoose.connect(url,{useNewUrlParser: true,useUnifiedTopology:true});
